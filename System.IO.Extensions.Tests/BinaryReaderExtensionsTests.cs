92c06205165795f7a15dd8f0450aa5cf99611982
Timur <dredpoll02@gmail.com>
2022-11-01 17:49:24 +0500

    #1 Improved unit tests to use [Theory] attribute

--- System.IO.Extensions.Tests/BinaryReaderExtensionsTests.cs
+++ System.IO.Extensions.Tests/BinaryReaderExtensionsTests.cs
@@ -1,93 +1,48 @@
+using System.Security.Cryptography;
+
 namespace System.IO.Extensions.Tests
 {
 	public class BinaryReaderExtensionsTests
 	{
-		[Fact]
-		public void ReadInt24()
+		[Theory]
+		[InlineData(0x4B4150, 0x50, 0x41, 0x4B)]
+		[InlineData(0x000000, 0x00, 0x00, 0x00)]
+		[InlineData(-1, 0xFF, 0xFF, 0xFF)]
+		public void ReadInt24(int expected, byte low, byte mid, byte high)
 		{
-			ReadOnlySpan<byte> bytes = stackalloc byte[] { 0x50, 0x41, 0x4B };
-			using var resourceReader = new BinaryReader(input: new MemoryStream(bytes.ToArray()));
+			byte[] buffer = new byte[] { low, mid, high, };
+			using var binaryReader = new BinaryReader(new MemoryStream(buffer));
 
-			int int24 = resourceReader.ReadInt24();
+			int actual = binaryReader.ReadInt24();
 
-			Assert.Equal(0x4B4150, int24);
+			Assert.Equal(expected, actual);
 		}
 
-		[Fact]
-		public void ReadInt24_FFFFFF_ReturnsMinusOne()
+		[Theory]
+		[InlineData(0x4B4150, 0x50, 0x41, 0x4B)]
+		[InlineData(0x000000, 0x00, 0x00, 0x00)]
+		[InlineData(0xFFFFFF, 0xFF, 0xFF, 0xFF)]
+		public void ReadUInt24(int expected, byte low, byte mid, byte high)
 		{
-			ReadOnlySpan<byte> bytes = stackalloc byte[] { 0xFF, 0xFF, 0xFF };
-			using var resourceReader = new BinaryReader(input: new MemoryStream(bytes.ToArray()));
+			byte[] buffer = new byte[] { low, mid, high, };
+			using var binaryReader = new BinaryReader(new MemoryStream(buffer));
 
-			int int24 = resourceReader.ReadInt24();
+			int actual = binaryReader.ReadUInt24();
 
-			Assert.Equal(-1, int24);
+			Assert.Equal(expected, actual);
 		}
 
-		[Fact]
-		public void ReadInt24_000000_ReturnsZero()
+		[Theory]
+		[InlineData("PAK", 0x50, 0x41, 0x4B, 0x00)]
+		[InlineData("PAKA", 0x50, 0x41, 0x4B, 0x41)]
+		public void ReadNullTerminatedString(string expected, byte low, byte lowmid, byte highmid, byte high)
 		{
-			ReadOnlySpan<byte> bytes = stackalloc byte[] { 0x00, 0x00, 0x00 };
-			using var resourceReader = new BinaryReader(input: new MemoryStream(bytes.ToArray()));
+			byte[] buffer = new byte[] { low, lowmid, highmid, high, };
+			using var binaryReader = new BinaryReader(new MemoryStream(buffer));
 
-			int int24 = resourceReader.ReadInt24();
+			string actual = binaryReader.ReadNullTerminatedString();
 
-			Assert.Equal(0x000000, int24);
-		}
-
-		[Fact]
-		public void ReadUInt24()
-		{
-			ReadOnlySpan<byte> bytes = stackalloc byte[] { 0x50, 0x41, 0x4B };
-			using var resourceReader = new BinaryReader(input: new MemoryStream(bytes.ToArray()));
-
-			int int24 = resourceReader.ReadUInt24();
-
-			Assert.Equal(0x4B4150, int24);
-		}
-
-		[Fact]
-		public void ReadUInt24_FFFFFF_ReturnsMaxValue()
-		{
-			ReadOnlySpan<byte> bytes = stackalloc byte[] { 0xFF, 0xFF, 0xFF };
-			using var resourceReader = new BinaryReader(input: new MemoryStream(bytes.ToArray()));
-
-			int int24 = resourceReader.ReadUInt24();
-
-			Assert.Equal(0xFFFFFF, int24);
-		}
-
-		[Fact]
-		public void ReadUInt24_000000_ReturnsZero()
-		{
-			ReadOnlySpan<byte> bytes = stackalloc byte[] { 0x00, 0x00, 0x00 };
-			using var resourceReader = new BinaryReader(input: new MemoryStream(bytes.ToArray()));
-
-			int int24 = resourceReader.ReadUInt24();
-
-			Assert.Equal(0x000000, int24);
-		}
-
-		[Fact]
-		public void ReadNullTerminatedString()
-		{
-			ReadOnlySpan<byte> bytes = stackalloc byte[] { 0x50, 0x41, 0x4B, 0x00 };
-			using var resourceReader = new BinaryReader(input: new MemoryStream(bytes.ToArray()));
-
-			string str = resourceReader.ReadNullTerminatedString();
-
-			Assert.Equal("PAK", str);
-		}
-
-		[Fact]
-		public void ReadNullTerminatedString_EndOfStream_ReadToEnd()
-		{
-			ReadOnlySpan<byte> bytes = stackalloc byte[] { 0x50, 0x41, 0x4B };
-			using var resourceReader = new BinaryReader(input: new MemoryStream(bytes.ToArray()));
-
-			string str = resourceReader.ReadNullTerminatedString();
-
-			Assert.Equal("PAK", str);
+			Assert.Equal(expected, actual);
 		}
 	}
 }
